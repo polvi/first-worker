7fb1d390375e49e318a2180c0d5835445f0970f7
Alex Polvi <alex@polvi.net>
2025-11-28 12:42:27 -0800

    bump

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,6 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello Branch!');
+		return new Response('Hello Branch 2!');
 	},
 } satisfies ExportedHandler<Env>;
