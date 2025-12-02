5070b82bff793ef8b68f1ea9a9621fbd13e41b23
Alex Polvi <alex@polvi.net>
2025-12-02 09:27:47 -0800

    new version

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,6 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello main 5!');
+		return new Response('Hello main 8!');
 	},
 } satisfies ExportedHandler<Env>;
